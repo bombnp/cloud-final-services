0789a52e33931b3e69a6035081e25732954739d2
KirkPig <KirkPig@github.com>
2022-05-08 09:13:03 +0700

    📝 config .gitignore for discord bot and add more in repo query services

--- api/repository/model.go
+++ api/repository/model.go
@@ -1,1 +1,7 @@
 package repository
+
+type Token struct {
+	Address string `gorm:"address"`
+	Symbol  string `gorm:"symbol"`
+	Icon    string `gorm:"icon"`
+}
--- api/repository/repository.go
+++ api/repository/repository.go
@@ -13,6 +13,30 @@ func New(pg *gorm.DB) *Databaser {
 }
 
 func (db *Databaser) InsertNewSubscribe(id, pool, t string) error {
+
 	query := `INSERT INTO pair_subscriptions (server_id,pool_address,type) VALUE('?','?','?')`
 	return db.Postgres.Exec(query, id, pool, t).Error
+
+}
+
+func (db *Databaser) QueryAllToken() ([]Token, error) {
+
+	var token_list []Token
+
+	query := `SELECT * FROM tokens`
+	err := db.Postgres.Raw(query).Scan(&token_list).Error
+
+	return token_list, err
+
+}
+
+func (db *Databaser) QueryToken(address string) (Token, error) {
+
+	var token Token
+
+	query := `SELECT * FROM tokens where address = ?`
+	err := db.Postgres.Raw(query, address).First(&token).Error
+
+	return token, err
+
 }
