74a7e7f61162fddfa38009418d2dee4aa269225b
KirkPig <KirkPig@github.com>
2022-05-15 03:14:32 +0700

    🚑 fix some format in discord bot

--- discord/commands/ping.ts
+++ discord/commands/ping.ts
@@ -2,13 +2,13 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("ping")
-		.setDescription("Test that the bot is still alive!"),
-	async execute(interaction: CommandInteraction) {
-		await interaction.reply({
-			content: "I am still alive!",
-			ephemeral: true,
-		});
-	},
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Test that the bot is still alive!"),
+    async execute(interaction: CommandInteraction) {
+        await interaction.reply({
+            content: "I am still alive!",
+            ephemeral: true,
+        });
+    },
 };
--- discord/commands/subscribe.ts
+++ discord/commands/subscribe.ts
@@ -2,8 +2,10 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder(),
-	async execute(interaction: CommandInteraction) {
-		await interaction.reply("I am still alive!");
-	},
+    data: new SlashCommandBuilder()
+        .setName("subscribe")
+        .setDescription("subscribe to alert bot"),
+    async execute(interaction: CommandInteraction) {
+        await interaction.reply("I am still alive!");
+    },
 };
