79e6654898c982d804afb9630dd7276493fa84e2
KirkPig <KirkPig@github.com>
2022-05-14 23:20:59 +0700

    🔥 fix command connection problem

---
+++ discord/commands/ping.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Test that the bot is still alive!"),
+	async execute(interaction: CommandInteraction) {
+		await interaction.reply("I am still alive!");
+	},
+};
--- discord/index.ts
+++ discord/index.ts
@@ -1,25 +1,92 @@
-import DiscordJS, { Intents } from "discord.js"
-import dotenv from "dotenv"
+import { Client, Intents, Interaction, Collection } from "discord.js";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
 
-dotenv.config()
+dotenv.config();
 
-const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
-})
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-client.on('ready', () => {
-    console.log("the bot is ready")
-})
+const commands = new Collection();
+const commandsData = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith(".ts"));
 
-client.on('messageCreate', (message) => {
-    if(message.content == "ping") {
-        message.reply({
-            content: "pong",
-        })
-    }
-})
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.set(command.data.name, command);
+	commandsData.push(command.data.toJSON());
+}
 
-client.login(process.env.TOKEN)
+client.once("ready", () => {
+	console.log("the bot is ready");
+
+	const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+
+	(async () => {
+		try {
+			if (process.env.ENV === "production") {
+				console.log(
+					"[production] Started refreshing application (/) commands."
+				);
+
+				await rest.put(
+					// @ts-ignore
+					Routes.applicationCommands(client.user.id),
+					{ body: commandsData }
+				);
+
+				console.log(
+					"[production] Successfully reloaded application (/) commands."
+				);
+			} else {
+				console.log(
+					"[test] Started refreshing application (/) commands."
+				);
+
+				await rest.put(
+					// @ts-ignore
+					Routes.applicationGuildCommands(
+						client.user.id,
+						process.env.GUILD_ID
+					),
+					{ body: commandsData }
+				);
+
+				console.log(
+					"[test] Successfully reloaded application (/) commands."
+				);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	})();
+});
+
+client.on("interactionCreate", async (interaction: Interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const command = commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		// @ts-ignore
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true,
+		});
+	}
+});
+
+client.login(process.env.TOKEN);
