e5326a845a07e22d115ef71c22f61d3620a322c2
KirkPig <KirkPig@github.com>
2022-05-14 21:30:07 +0700

    🚀 updated model

--- api/repository/model.go
+++ api/repository/model.go
@@ -1,14 +1,14 @@
 package repository
 
 type Token struct {
-	Address string `gorm:"address"`
-	Symbol  string `gorm:"symbol"`
-	Icon    string `gorm:"icon"`
+	Address string `gorm:"column:address"`
+	Symbol  string `gorm:"column:symbol"`
+	Icon    string `gorm:"column:icon"`
 }
 
 type Pair struct {
-	PoolAddress  string `gorm:"pool_address"`
-	BaseAddress  string `gorm:"base_address"`
-	QuoteAddress string `gorm:"quote_address"`
-	IsBaseToken0 bool   `gorm:"is_base_token0"`
+	PoolAddress  string `gorm:"column:pool_address"`
+	BaseAddress  string `gorm:"column:base_address"`
+	QuoteAddress string `gorm:"column:quote_address"`
+	IsBaseToken0 bool   `gorm:"column:is_base_token0"`
 }
