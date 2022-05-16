78781801744e30fc01a232f156b496cfaeef8263
KirkPig <KirkPig@github.com>
2022-05-16 12:47:22 +0700

    fix command suggestion

--- discord/commands/ping.ts
+++ discord/commands/ping.ts
@@ -5,6 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Test that the bot is still alive!"),
+    async init() {},
     async execute(interaction: CommandInteraction) {
         await interaction.reply({
             content: "I am still alive!",
